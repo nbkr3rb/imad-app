bad4e7f0df28dfc481a99ff7be967cd3cda4bd90
nbkr3rb <36580009+nbkr3rb@users.noreply.github.com>
2018-02-19 15:41:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-two', function(req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
 });
 app.get('/article-three', function(req, res) {
-   res.send('Article three requested and will be served here'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
