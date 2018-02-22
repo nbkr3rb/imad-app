85c3dfd9980eee68b0cd900975022b75719cf1f8
nbkr3rb <36580009+nbkr3rb@users.noreply.github.com>
2018-02-22 21:18:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 // Move the image
 var img = document.getElementById('madi');
+var marginLeft = o;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + px;
+}
 img.onclick = function(){
+    var interval = setInterval(moveRight, 100)
     img.style.marginLeft = "100px";
 };
